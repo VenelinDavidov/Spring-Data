68da8dd8d28e26ece4479cfb9d0822ddd124b752
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2024-10-13 22:34:13 +0300

    ORM Fundamentals
    • Introduction to ORM;
    • Architecture;
    • Example;
    • Approaches;
    • ORM Advantages.

---
+++ SpringData/src/ORM_Fundamentals/Lab_ORM Fundamentals/mini_orm/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import bg.softuni.Order;
+import bg.softuni.User;
+import orm.EntityManager;
+import orm.MyConnector;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+
+        MyConnector.createConnection ("root", "Root", "mini_orm");
+
+        Connection connection = MyConnector.getConnection ();
+
+        EntityManager <User> userEntityManager = new EntityManager <> (connection);
+        User pesho = new User ("pesho", 45, LocalDate.now ());
+        pesho.setId (1);
+        userEntityManager.persist (pesho);
+
+        User users = userEntityManager.findFirst (User.class);
+
+        System.out.println (users);
+
+//        EntityManager<Product> productEntityManager = new EntityManager<>(connection);
+//        Product pen = new Product("pen", 2.34);
+//        productEntityManager.persist(pen);
+
+        EntityManager <Order> orderEntityManager = new EntityManager <> (connection);
+        Order order = new Order ("mn123b4", LocalDate.now ());
+        orderEntityManager.persist (order);
+
+    }
+
+}
